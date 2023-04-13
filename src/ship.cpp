6ea75bb513524951a9cb7ca3e3f8c86440b64d3b
Inkl1ng <scottbot2018@gmail.com>
2023-04-12 22:55:54 -0700

    finished Ship class constructor

--- src/ship.cpp
+++ src/ship.cpp
@@ -1,10 +1,44 @@
 #include "ship.hpp"
 #include <iostream>
+#include <array>
+#include <vector>
+
+std::vector<std::array<int, 2>>& Ship::getLocations()
+{
+    return this->locations;
+}
 
 Ship::Ship(char type, std::array<int, 2> loc, char orientation, int size)
 {
     this->type = type;
     this->loc = loc;
     this->orientation = orientation;
-    // this->size = size;
+    this->size = size;
+
+    // calculate locations
+    int colOrigin { loc[0] };
+    int rowOrigin { loc[1] };
+    switch (orientation)
+    {
+        case 'h':
+            for (int row { rowOrigin }; row < rowOrigin + this->size; ++row)
+            {
+                this->locations.push_back({row, colOrigin});
+            }
+            break;
+        case 'v':
+            for (int col { colOrigin}; col < colOrigin + this->size; ++col)
+            {
+                this->locations.push_back({col, rowOrigin});
+            }
+            break;
+    }
+}
+
+int main()
+{
+    std::cout << "Hello\n";
+    Ship testShip ('c', {5, 5}, 'h', 3);
+    std::cin.ignore();
+    return 0;
 }
--- src/ship.hpp
+++ src/ship.hpp
@@ -3,17 +3,20 @@
 
 #include <iostream>
 #include <array>
+#include <vector>
 
 class Ship
 {
     public:
         // ship class constructor
         Ship(char type, std::array<int, 2> loc, char orientation, int size);
+        std::vector<std::array<int, 2>>& getLocations();
     private:
         char type {};
-        std::array<int, 2> loc {};
+        std::array<int, 2> loc {}; // (x, y) or (col, row)
         char orientation {}; // 'h' for horizontal, 'v' for vertical
-
+        int size {};
+        std::vector<std::array<int, 2>> locations {};
 };
 
 #endif
