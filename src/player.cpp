ea20a0ce5aa92de244c2c844309dc9b0d3e67e00
Inkl1ng <scottbot2018@gmail.com>
2023-04-17 00:36:29 -0700

    started work on registering shots

--- src/player.cpp
+++ src/player.cpp
@@ -1,4 +1,5 @@
 #include "player.hpp"
+#include "types.hpp"
 #include <iostream>
 
 Player::Player(std::string playerName)
@@ -28,4 +29,23 @@ char Player::getOceanPiece(int row, int col)
 char Player::getTargetPiece(int row, int col)
 {
     return this->targetBoard[row][col];
+}
+
+bool Player::shoot(int row, int col, Player &target)
+{
+    // input is 100% legal when it is passed through this method
+    // check if the shot was a hit
+    if (target.getOceanPiece(row, col) != '.')
+    {
+        // hit
+    }
+    else
+    {
+        // miss
+    }
+    // if a hit check what kind
+    
+    
+
+    return false;
 }
