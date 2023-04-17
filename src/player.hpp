a38e31c50b4aee0e2ed69797ae1dd2762bfb2488
Inkl1ng <scottbot2018@gmail.com>
2023-04-17 00:36:57 -0700

    added shoot method and reorganized class

--- src/player.hpp
+++ src/player.hpp
@@ -1,30 +1,34 @@
-#ifndef PLAYER_H
-#define PLAYER_H
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
 
+#include "types.hpp"
 #include <iostream>
+#include <string>
 #include <array>
 
 // class to represent players
 // contains name, boards, and ships
 class Player
 {
-    std::string name{};
-    // 2d std::array to represent where the player has placed their
-    // ships and where the ships have been hit
-    std::array<std::array<char, 10>, 10> oceanBoard {};
-    // 2d std::array to represent where the player has shot, hit, and missed
-    std::array<std::array<char, 10>, 10> targetBoard {};
+    public:
+        // Player class constructor
+        Player(std::string playerName);
 
-public:
-    // Player class constructor
-    Player(std::string playerName);
+        // returns reference to the player's name
+        std::string & getName();
+        // get piece at (row, col) in oceanBorad
+        char getOceanPiece(int row, int col);
+        // get piece at (row, col) in targetBoard
+        char getTargetPiece(int row, int col);
+        bool shoot(int row, int col, Player &target);
 
-    // returns reference to the player's name
-    std::string & getName();
-    // get piece at (row, col) in oceanBorad
-    char getOceanPiece(int row, int col);
-    // get piece at (row, col) in targetBoard
-    char getTargetPiece(int row, int col);
+    private:
+        std::string name{};
+        // represents where the player has placed their ships and where the
+        // ships have been hit
+        Board oceanBoard {};
+        // represents where the player has shot, hit, and missed
+        Board targetBoard {};
 };
 
 #endif
