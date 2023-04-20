63ac02cb6b7f3c42b905dede29570ea5b9a70582
Inkl1ng <scottbot2018@gmail.com>
2023-04-19 21:36:51 -0700

    finished placement code, needs testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,79 @@
 #include "player.hpp"
 #include "render.hpp"
+#include "ship.hpp"
 #include <iostream>
 
+bool checkInput(int input)
+{
+    if (input < 0 || input > 10)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+
+void placeShips(Player &player, bool isPlayer2)
+{
+    std::array<Ship, 5> unplacedShips {
+        Ship("carrier", 5),
+        Ship("battleship", 4),
+        Ship("destroyer", 3),
+        Ship("submarine", 3),
+        Ship("patrol boat", 2)
+    };
+
+    int rowInput{};
+    int colInput{};
+    char orientationInput{};
+    bool validPlacement{ false };
+    std::string placementMessage{};
+
+    for (int i{ 0 }; i < 5; ++i)
+    {
+        // render board
+        Render::renderBoard(player, isPlayer2);
+        // get placements
+        placementMessage = unplacedShips[i].getType() + " placement\n";
+        Render::message(placementMessage, isPlayer2);
+        while (!validPlacement)
+        {
+            rowInput = Render::ask<int>("Row (1-10): ", isPlayer2) - 1;
+            colInput = Render::ask<int>("Column (1-10): ", isPlayer2) - 1;
+            orientationInput = Render::ask<char>("Orientation ('h' or 'v'): ", isPlayer2);
+            orientationInput = tolower(orientationInput);
+
+            // validate placements
+            // legal row/col
+            validPlacement = checkInput(rowInput);
+            // this repeated snippet breaks the loop is one of the inputs is bad
+            if (!validPlacement) { std::cout << "Invalid inputs!\n"; continue; }
+            validPlacement = checkInput(colInput);
+            if (!validPlacement) { std::cout << "Invalid inputs!\n"; continue; }
+            
+            // valid orientation (either h or v)
+            validPlacement = orientationInput == 'h' or orientationInput == 'v';
+            if (!validPlacement) { std::cout << "Invalid inputs!\n"; continue; }
+            
+            // check if there are already ships there
+            unplacedShips[i].setLoc(colInput, rowInput);
+            unplacedShips[i].setOrientation(orientationInput);
+            unplacedShips[i].updateLocations();
+
+            for (std::array<int, 2> location : unplacedShips[i].getLocations())
+            {
+                validPlacement = player.getOceanPiece(location[0], location[1]) != '.';
+                if (!validPlacement) { std::cout << "Invalid inputs!\n"; continue; }
+            }
+        }
+    }
+}
+
 // handles main gameloop
-bool play_battleship()
+bool playBattleship()
 {
     // ask for names
     std::cout << "Player 1, enter your name: ";
@@ -20,11 +90,15 @@ bool play_battleship()
     Player player1(player1Name);
     Player player2(player2Name);
 
+    // place ships
+    placeShips(player1, false);
+    placeShips(player2, true);
+
     // main game loop
-    char row {};
-    int col {};
-    bool playing { true };
-    bool validInput { false };
+    char row{};
+    int col{};
+    bool playing{ true };
+    bool validInput{ false };
     while (playing)
     {
         // render
@@ -41,18 +115,8 @@ bool play_battleship()
             col--; // rows go from 1-10 but array is 0-9
 
             // validate input for valid location
-            if (row < 0 || row > 9)
-            {
-                validInput = false;
-            }
-            else if (col < 0 || col > 9)
-            {
-                validInput = false;
-            }
-            else
-            {
-                validInput = true;
-            }
+            validInput = checkInput(row);
+            validInput = checkInput(col);
 
             // check if the player has already shot where they want to shoot
             if (player1.getTargetPiece(row, col) == '.')
@@ -89,16 +153,20 @@ int main()
 {
     std::cout << "Battleship!\n\n";
     std::cout << "Play game (Y/N)?: ";
-    char play_game;
-    std::cin >> play_game;
-
-    bool keepPlaying { true };
+    char playGame;
+    std::cin >> playGame;
+    playGame = tolower(playGame);
+    bool keepPlaying{ true };
 
     while (keepPlaying)
     {
-        if (tolower(play_game) == 'y')
+        if (tolower(playGame) == 'y')
         {
-            keepPlaying = play_battleship();
+            keepPlaying = playBattleship();
+        }
+        else
+        {
+            keepPlaying = false;
         }
     }
 
