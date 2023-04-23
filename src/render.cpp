3cb36259f174390950e5746402275dadf2615fe4
Inkl1ng <scottbot2018@gmail.com>
2023-04-23 12:53:29 -0700

    fix: fix renderBoardRow rendering an extra space for targetBoards

--- src/render.cpp
+++ src/render.cpp
@@ -42,25 +42,10 @@ void Render::renderBoardRow(Player &player, char type, int row)
         }
         else if (type == 't')
         {
-            std::cout << player.getTargetPiece(row, col) << ' ';
+            std::cout << player.getTargetPiece(row, col);
         }
         std::cout << ' ';
     }
-    // switch (type)
-    // {
-    //     case 'o':
-    //         for (int col { 0 }; col < 10; ++col)
-    //         {
-    //             std::cout << player.getOceanPiece(row, col) << ' ';
-    //         }
-    //         break;
-    //     case 't':
-    //         for (int col { 0 }; col < 10; ++col)
-    //         {
-    //             std::cout << player.getTargetPiece(row, col) << ' ';
-    //         }
-    //         break;            
-    // }
 }
 
 /*
